fb455f24b1cf6c22a51dd0e8c81accb58de66f3b
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2015-09-10 15:49:18 -0400

    Add manage command for things like migrations

--- manage.py
+++ manage.py
@@ -35,23 +35,10 @@ try:
     )
     django.setup()
 
-    from django_nose import NoseTestSuiteRunner
 except ImportError:
     raise ImportError("Ensure this is run in an environment with Django and test requirements installed.")
 
 
-def run_tests(*test_args):
-    if not test_args:
-        test_args = ['tests']
-
-    # Run tests
-    test_runner = NoseTestSuiteRunner(verbosity=1)
-
-    failures = test_runner.run_tests(test_args)
-
-    if failures:
-        sys.exit(failures)
-
-
 if __name__ == '__main__':
-    run_tests(*sys.argv[1:])
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
