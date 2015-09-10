1f5f8ff3f97612980059eb73744cc96fdb5e786a
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2015-09-10 16:22:17 -0400

    Remove try/except clause

--- manage.py
+++ manage.py
@@ -6,41 +6,37 @@ Management command entry point for working with migrations
 """
 
 import sys
+import django
+from django.conf import settings
 
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    "firstclass",
+]
+
+if django.VERSION[1] < 7:
+    INSTALLED_APPS = ['south'] + INSTALLED_APPS
+
+settings.configure(
+    DEBUG=True,
+    USE_TZ=True,
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+        }
+    },
+    ROOT_URLCONF="firstclass.urls",
+    INSTALLED_APPS=INSTALLED_APPS,
+    MIDDLEWARE_CLASSES=(),  # Silence Django 1.7 warnings
+    SITE_ID=1,
+)
 try:
-
-    import django
-    from django.conf import settings
-    INSTALLED_APPS = [
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sites",
-        "firstclass",
-    ]
-
-    if django.VERSION[1] < 7:
-        INSTALLED_APPS = ['south'] + INSTALLED_APPS
-
-    settings.configure(
-        DEBUG=True,
-        USE_TZ=True,
-        DATABASES={
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-            }
-        },
-        ROOT_URLCONF="firstclass.urls",
-        INSTALLED_APPS=INSTALLED_APPS,
-        MIDDLEWARE_CLASSES=(),  # Silence Django 1.7 warnings
-        SITE_ID=1,
-    )
-    try:
-        django.setup()
-    except AttributeError:
-        pass
-
-except ImportError:
-    raise ImportError("Ensure this is run in an environment with Django and test requirements installed.")
+    django.setup()
+except AttributeError:
+    # Django 1.4
+    pass
 
 
 if __name__ == '__main__':
